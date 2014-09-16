3f909fd55f1c5fc9156f62442997084d0f342845
victoroff <mrbebz@gmail.com>
2014-09-17 00:23:51 +0300

    made FuelContainer

--- Racecar/src/FuelContainer.java
+++ Racecar/src/FuelContainer.java
@@ -4,14 +4,21 @@ import java.awt.*;
 public class FuelContainer {
 
 	int x, y;
-
+	//Bebbo: make FuelContainer repeat;
+		private int startY;
+		
 	public FuelContainer(int x, int y) {
 		this.x = x;
 		this.y = y;
+		//Bebbo: make FuelContainer repeat
+		startY = y;
 	}
 
     public void update() {
     	y += 1; //edited by vlado - continues move
+    	
+    	//Bebbo: make FuelContainer repeat
+    	checkOffScreen();
     }
 
     public void draw(Graphics2D g2d) {
@@ -27,4 +34,12 @@ public class FuelContainer {
     public Rectangle getBounds() {
         return new Rectangle(x, y, getFuelContainerImg().getWidth(null), getFuelContainerImg().getHeight(null));
     }
+    
+  //Bebbo: make FuelContainer repeat;
+    public void checkOffScreen(){
+		
+		if (y >= 680) {
+			y = startY;
+		}
+	}
 }
